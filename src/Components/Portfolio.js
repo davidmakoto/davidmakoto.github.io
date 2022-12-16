b8ede7811a65f8a3090be9772256be76eb2222dd
dmw <davidmacotoward@gmail.com>
2022-12-15 19:50:11 -0800

    init

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,6 +1,22 @@
 import React, { Component } from "react";
+// import Card from "./Components/Card/Card.babel"; // ./Components/Card";
+
 import Zmage from "react-zmage";
 import Fade from "react-reveal";
+
+
+
+// class Button extends React.Component {
+//   btnClick() {
+//     window.open("https://www.google.com");
+//   }
+// render() {
+//     return (<div>
+//             <ButtonComponent cssClass='e-link' onClick={this.btnClick.bind(this)}>Go to google</ButtonComponent>
+//         </div>);
+//   }
+// }
+// ReactDOM.render(<App />, document.getElementById('button'));
 
 let id = 0;
 class Portfolio extends Component {
@@ -14,18 +30,39 @@ class Portfolio extends Component {
         <div key={id++} className="columns portfolio-item">
           <div className="item-wrap">
             <Zmage alt={projects.title} src={projectImage} />
-            <div style={{ textAlign: "center" }}>{projects.title}</div>
+            <div style={{ textAlign: "center" }}>
+              <h2><a href={projects.url}>{projects.title}</a></h2>
+              <div className="languages">
+                <p>{projects.languages} </p> 
+              </div>
+              <div className="years">
+                <p>{projects.years} </p> 
+              </div>
+              <b>
+                {projects.description} 
+              </b> 
+            </div>
+            <ul className="social">
+                {/* <a href={project} className="button btn project-btn">
+                  <i className="fa fa-book"></i>Project
+                </a> */}
+              <a href={projects.url} className="button btn github-btn">
+                  <i className="fa fa-github"></i>Github
+                </a>
+              </ul>
+            {/* <div style={{ textAlign: "center" }}>{projects.url}</div> */}
+
           </div>
         </div>
       );
     });
 
     return (
-      <section id="portfolio">
+    <section id="portfolio"> 
         <Fade left duration={1000} distance="40px">
           <div className="row">
             <div className="twelve columns collapsed">
-              <h1>Check Out Some of My Works.</h1>
+              <h1>Projects</h1>
 
               <div
                 id="portfolio-wrapper"
