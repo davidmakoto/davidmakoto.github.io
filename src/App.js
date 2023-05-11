16b1e45235646e9571bbf2e2c19dcf0243cfd788
David <davidmacotoward@gmail.com>
2023-05-11 09:01:44 -0700

    new resume and reworded summaries

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import About from "./Components/About";
 import Resume from "./Components/Resume";
 import Contact from "./Components/Contact";
 import Portfolio from "./Components/Portfolio";
+import Awards from "./Components/Awards";
 
 class App extends Component {
   constructor(props) {
@@ -47,6 +48,7 @@ class App extends Component {
         <About data={this.state.resumeData.main} />
         <Resume data={this.state.resumeData.resume} />
         <Portfolio data={this.state.resumeData.portfolio} />
+        {/* <Awards data={this.state.resumeData.awards} /> */}
         <Contact data={this.state.resumeData.main} />
         <Footer data={this.state.resumeData.main} />
       </div>
